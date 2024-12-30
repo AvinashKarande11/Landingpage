c9c8d467b2b6d6134dc71260c27e8eb96f8eb2aa
Avinash Karande <162133683+AvinashKarande11@users.noreply.github.com>
2024-12-30 11:54:54 +0530

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,9 @@
+// Open the side navigation bar
+function openNavbar() {
+    document.getElementById("sideNavigationBar").classList.add('open');
+}
+
+// Close the side navigation bar
+function closeNavbar() {
+    document.getElementById("sideNavigationBar").classList.remove('open');
+}
